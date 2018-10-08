d6fd72ecf671ced83b093b812b7d7fd362d07e73
Sébastien Portebois <sportebois@gmail.com>
2018-10-08 13:57:13 -0400

    Clean comments

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,8 @@ ADD haproxy.conf /etc/rsyslog.d
 ADD rsyslog.conf /etc/rsyslog.conf
 COPY docker-entrypoint.sh /
 
+# Optional ENV var you could override:
+# HAPROXY_CFG_FILE setup the full path (in the container) where to load the HAproxy config
 
 EXPOSE 80 443 1883 8883 1988
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+
 # Optional ENV VAR:
 #  HAPROXY_CFG_FILE  the absolute path to the HAPRoxy.cfg file you want to use
 #    defaults to /usr/local/etc/haproxy/haproxy.cfg if unset
-PID_FILE="/var/run/haproxy.pid"
 
+PID_FILE="/var/run/haproxy.pid"
 
 # Make sure service is running
 service rsyslog start
@@ -14,18 +15,7 @@ touch /var/log/haproxy.log
 # Throw the log to output
 tail -f /var/log/haproxy.log &
 
-
-#function reload
-#{
-#  haproxy -f <config> -p <pidfile> -sf $(cat <pidfile>) &
-#  wait
-#}
-#
-#trap reload SIGHUP
-#trap "kill -TERM $(cat pidfile) || exit 1" SIGTERM SIGINT
-
 # Start haproxy
-#   -W  -- "master-worker mode" (similar to the old "haproxy-systemd-wrapper"; allows for reload via "SIGUSR2")
 exec haproxy -W \
   -f "${HAPROXY_CFG_FILE:-/usr/local/etc/haproxy/haproxy.cfg}" \
   -p $PID_FILE \
